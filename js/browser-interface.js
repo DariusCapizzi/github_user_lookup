9407c55dc46a7cc510eb1432863f101fb15f95f7
DariusCapizzi <DariusCapizzi@gmail.com>
2016-05-27 08:47:00 -0700

    add basic functionality and integration for github api

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,6 +1,12 @@
-var TestFunction = require('./../js/function.js');
+var gitApiToken = require('./../.env').gitApiToken;
+var searchUser = require('./../js/scripts.js').searchUser;
 
 
 $(function() {
-
+  $.get("https://api.github.com/users/" + "DariusCapizzi" + "/repos?access_token=" + gitApiToken).then(function(response){
+    var newUser = new searchUser(response);
+    console.log(newUser.avatar);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
 });
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+
+exports.searchUser = function(response) {
+  this.avatar = response[0].owner.avatar_url;
+	this.owner = response[0].owner.login;
+	this.gitLink = response[0].owner.html_url;
+	this.gistUrl = response[0].gist_url;
+};
