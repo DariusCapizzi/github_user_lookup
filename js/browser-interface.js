0999b4553dbef448dfadc351c04b0ce2ae2e0673
DariusCapizzi <DariusCapizzi@gmail.com>
2016-05-27 14:42:20 -0700

    add page number functionality

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,12 +1,45 @@
-var gitApiToken = require('./../.env').gitApiToken;
-var searchUser = require('./../js/scripts.js').searchUser;
-
+var BaseUser = require('./../js/scripts.js').BaseUser;
 
 $(function() {
-  $.get("https://api.github.com/users/" + "DariusCapizzi" + "/repos?access_token=" + gitApiToken).then(function(response){
-    var newUser = new searchUser(response);
-    console.log(newUser.avatar);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
+  $("#search-user").submit(function(e){
+    e.preventDefault();
+    $('#user-image, #owner-info, .repositories, .repo').empty();
+
+
+		var userQuery = $('#user-query').val();
+    var baseUser = new BaseUser();
+
+    baseUser.sendQuery(userQuery, function(searchedUser){
+      $("#user-image").append('<img class="avatar-img" src=' + searchedUser.avatar + '>')
+      $('#owner-info').append('<a href=' + searchedUser.gitLink  + ">" + searchedUser.owner + "</a>");
+      for (i=0;i<searchedUser.repos.length;i++){
+        $(".repositories").show();
+        $('.repositories').append('<div class="repo"><a href=' + searchedUser.repos[i].html_url + ">" + searchedUser.repos[i].name + "</a></div>");
+      }
+
+
+      for (i=1; i < (parseInt(searchedUser.pages.last.page) + 1); i++) {
+        $("#page-numbers").append("<li><a class='page'>" + i + "</a></li>");
+      }
+
+      console.log(searchedUser.pages.last.page);
+
+      $(".page").click(function(e){
+        e.preventDefault();
+        console.log(this.innerHTML)
+        baseUser.sendQuery(userQuery, function(searchedUser){
+          $('.repositories, .repo').empty();
+          for (i=0;i<searchedUser.repos.length;i++){
+            $(".repositories").show();
+            $('.repositories').append('<div class="repo"><a href=' + searchedUser.repos[i].html_url + ">" + searchedUser.repos[i].name + "</a></div>");
+          }
+        }, this.innerHTML);
+      });
+    }, 1);
+
+    $("#user-query").val("");
+  })
+
+
+
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
+var gitApiToken = require('./../.env').gitApiToken;
+var parse = require('parse-link-header');
 
 
-exports.searchUser = function(response) {
+exports.BaseUser = function() {
+};
+
+SearchedUser = function(response, xhr) {
   this.avatar = response[0].owner.avatar_url;
 	this.owner = response[0].owner.login;
 	this.gitLink = response[0].owner.html_url;
-	this.gistUrl = response[0].gist_url;
+	this.created_at = response[0].created_at;
+  this.repos = response;
+  this.pages = parse(xhr.getResponseHeader('link'));
+};
+
+exports.BaseUser.prototype.sendQuery = function (userQuery, callback, page) {
+  $.get("https://api.github.com/users/" + userQuery + "/repos?page=" + page + "&access_token=" + gitApiToken).then(function(response, status, xhr){
+    var searchedUser = new SearchedUser(response, xhr);
+    callback(searchedUser);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
 };
